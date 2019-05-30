deed0c259b8e387b9e55e3e1c37c7a4971f5b0fe
Diego Rojo <diego2259@gmail.com>
2019-05-30 10:54:52 -0300

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class CommentsController < ApplicationController
       before_action :set_comment, only: %i[show update destroy]
-      # before_action :authenticate_user!
+      before_action :authenticate_user!
 
       # GET /comments
       def index
