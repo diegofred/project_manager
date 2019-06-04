40ea825ffece38c5d1c8350f86bb567790ee4806
Diego Rojo <diego2259@gmail.com>
2019-06-03 22:17:57 -0400

    add some test cases

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,22 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Projects', type: :request do
-<<<<<<< HEAD
-  context 'context: request via API' do
-    include_context 'session_api'
-    before(:each) do
-      @current_user = FactoryBot.create(:user)
-      @project = FactoryBot.create(:project, user_id: @current_user.id)
-    end
-
-    describe 'GET /api/v1/projects' do
-      it 'works! (now write some real specs)' do
-        login
-        auth_params = get_auth_params_from_login_response_headers(response)
-        get api_v1_projects_path, headers: auth_params
-        expect(response).to have_http_status(200)
-      end
-=======
   include_context 'session_api'
   before(:each) do
     @current_user = FactoryBot.create(:user)
@@ -28,7 +12,6 @@ RSpec.describe 'Projects', type: :request do
       auth_params = get_auth_params_from_login_response_headers(response)
       get api_v1_projects_path, headers: auth_params
       expect(response).to have_http_status(200)
->>>>>>> 17c922e3a3ed8b4b19bcc268507202a33c64726c
     end
   end
 end
